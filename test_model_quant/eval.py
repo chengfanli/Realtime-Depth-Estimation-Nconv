loss units corrected

test_model_quant/eval.py
@@ -79,17 +79,15 @@ class Evaluator():
         avg_imae_loss = 0
         highest_imae_loss = 0
 
-        #TODO: Remove Outliers
-        #TODO: Convert units to mm
-
-
-
         for i in range(len(self.our_images)):
             our_image = torch.tensor(self.our_images[i] * 1000, dtype=torch.float32)
             gt_image = torch.tensor(self.gt_images[i] * 1000, dtype=torch.float32)
-            # inv_our_image = 1.0 / (our_image + 1e-6)
-            # inv_gt_image = 1.0 / (gt_image + 1e-6)
 
+            #note that these images were originally in m and we convert them
+            #to mm for loss calculations
+
+
+            #convert to km for inverse loss calculations
             inv_our_image = 1e+6 / (our_image + 1e-6)
             inv_gt_image = 1e+6 / (gt_image + 1e-6)    
 

test_model_quant/test_model.py
@@ -16,7 +16,7 @@ from scipy.ndimage import median_filter
 from models.without_step1.step1 import SETP1_NCONV as step_1
 
 #Modify this
-checkpoint_path = "baseline2"
+checkpoint_path = "Test"
 output_folder = "ours"
 
 test_dataset = TEST_DataLoader_NYU('/oscar/data/jtompki1/cli277/new_spot_data', '1')